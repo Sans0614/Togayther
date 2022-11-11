268cc27cf4602f03c95912b1e82cd162c35955f4
Sans <rebound19960614@gmail.com>
2022-11-11 18:04:32 +0800

    20221111-02 advertise-fixed update

--- js/navbar.js
+++ js/navbar.js
@@ -65,100 +65,10 @@ $("[data-hover-text]").mouseleave(function(){
 });
 
 
-// /*熱門主打*/
-// $(".cate01").mouseenter(function(){
-//   $(".cate01").text("發燒話題");
-// });
-
-// $(".cate01").mouseleave(function(){
-//   $(".cate01").text("HOT ISSUE");
-// });
-
-// /*精選頻道*/
-// $(".cate02").mouseenter(function(){
-//   $(".cate02").text("精選頻道");
-// });
-
-// $(".cate02").mouseleave(function(){
-//   $(".cate02").text("YOUTUBE");
-// });
-
-// /*品味職人*/
-// $(".cate03").mouseenter(function(){
-//   $(".cate03").text("品味職人");
-// });
-
-// $(".cate03").mouseleave(function(){
-//   $(".cate03").text("CRAFTSMAN");
-// });
-
-// /*影音娛樂*/
-// $(".cate04").mouseenter(function(){
-//   $(".cate04").text("影音娛樂");
-// });
-
-// $(".cate04").mouseleave(function(){
-//   $(".cate04").text("MEDIA ENTERTAINMENT");
-// });
-
-// /*生活消費*/
-// $(".cate05").mouseenter(function(){
-//   $(".cate05").text("生活消費");
-// });
-
-// $(".cate05").mouseleave(function(){
-//   $(".cate05").text("LIVING CONSUMPTION");
-// });
-
-// /*美食旅遊*/
-// $(".cate06").mouseenter(function(){
-//   $(".cate06").text("美食旅遊");
-// });
-
-// $(".cate06").mouseleave(function(){
-//   $(".cate06").text("FOOD&TRAVEL");
-// });
-
-// /*潮流時尚*/
-// $(".cate07").mouseenter(function(){
-//   $(".cate07").text("潮流時尚");
-// });
-
-// $(".cate07").mouseleave(function(){
-//   $(".cate07").text("FASHION");
-// });
-
-// /*運動健康*/
-// $(".cate08").mouseenter(function(){
-//   $(".cate08").text("運動健康");
-// });
-
-// $(".cate08").mouseleave(function(){
-//   $(".cate08").text("SPORTS");
-// });
-
-// /*友善連結*/
-// $(".cate09").mouseenter(function(){
-//   $(".cate09").text("友善連結");
-// });
-
-// $(".cate09").mouseleave(function(){
-//   $(".cate09").text("LINKS");
-// });
-
-// function navbarfixed(){
-//   var scroll_y = $(window).scrollTop();
-//   var scroll_y_trig = 1;
-//   if(scroll_y > scroll_y_trig){
-//     $('#navbar').css('position','fixed');
-//   }
-//   if(scroll_y < scroll_y_trig){
-//     $('#navbar').css('position','relative');
-//   }
-// }
-
-// $(document).ready(function(){
-//   $(window).scroll(function(){
-//     navbarfixed();
-//   })
-// })
+//固定廣告關閉功能
+$(function(){
+  $('#advertise-fixed-close-button').on('click',function(){
+    $('#advertise-fixed-button').css('display','none')
+    $('#advertise-fixed-close-button').css('display','none')
+  });
+});
